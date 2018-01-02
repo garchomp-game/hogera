80321fe4d5fc60d9536f4d78c586a83ae4feeb30
garchomp-game <otoka.japanesedrum@gmail.com>
2018-01-02 18:33:54 +0900

    first-commit

---
+++ hoge/index.php
@@ -0,0 +1,13 @@
+<?php
+echo 'hello'
+ ?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>hoge</title>
+   </head>
+   <body>
+     <h1>hoge</h1>
+   </body>
+ </html>
